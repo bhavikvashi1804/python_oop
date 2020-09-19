ade699ce22ce76cc6db83af34011e0467975639c
Bhavik Vashi <bhavikvashi1804@gmail.com>
2020-09-19 14:31:04 +0530

    Actual use of init method

--- init_method.py
+++ init_method.py
@@ -1,10 +1,13 @@
 class Student:
     
-    def __init__(self):
-        self.name= "Bhavik Vashi"
+    def __init__(self,name):
+        self.name= name
 
     def displayStudentDetails(self):
         print("Student name",self.name)
 
-studentOne = Student()
+studentOne = Student("Bhavik Vashi")
 studentOne.displayStudentDetails()
+
+studentTwo = Student("Raj Patel")
+studentTwo.displayStudentDetails()
