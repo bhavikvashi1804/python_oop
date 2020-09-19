6b200b8623bf346c2a84df9105e71cf3a86490aa
Bhavik Vashi <bhavikvashi1804@gmail.com>
2020-09-19 18:26:52 +0530

    Operator Overloading problem solve

--- Polymorphism/operator_Overloading.py
+++ Polymorphism/operator_Overloading.py
@@ -2,8 +2,8 @@ class square:
     def __init__(self,side):
         self.side=side
 
-    def __add__(square1,square2):
-        return ((4*square1.side) + (4*square2.side))
+    def __add__(self,s1):
+        return ((4*self.side) + (4*s1.side))
 
 square1 = square(5)
 square2 = square(10)
